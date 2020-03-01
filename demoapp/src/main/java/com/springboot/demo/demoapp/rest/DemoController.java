5968dfb88b01cf240c6852eb8bbf1900dfc42be3
rohit2927 <rohit2927@gmail.com>
2020-03-02 00:16:37 +0530

    initial commit

---
+++ demoapp/src/main/java/com/springboot/demo/demoapp/rest/DemoController.java
@@ -0,0 +1,15 @@
+package com.springboot.demo.demoapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hello World !! Time on server is "+ LocalDateTime.now();
+	}
+}
